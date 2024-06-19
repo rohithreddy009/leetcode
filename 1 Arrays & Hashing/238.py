5bc6233cb41dc490334f91e15b86b1695fb9561f
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-06-19 23:14:23 +0530

    36

--- 1 Arrays & Hashing/238.py
+++ 1 Arrays & Hashing/238.py
@@ -12,14 +12,18 @@ class Solution:
         #     ans[i] = product
         # return ans
 
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
 
-        for i in range(1, len(nums)):
-            res[i] = res[i-1] * nums[i-1]
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
         postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
+        for i in range(len(nums) -1, -1, -1):
+            res[i] *= postfix   
             postfix *= nums[i]
         return res
 
