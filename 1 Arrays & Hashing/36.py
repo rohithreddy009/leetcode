aa887a88f328f9ff6bac71c1c87125ed2c36aa48
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-06-20 01:31:13 +0530

    36

---
+++ 1 Arrays & Hashing/36.py
@@ -0,0 +1,35 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        square = defaultdict(set)
+
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == ".":
+                    continue
+                if (board[row][col] in rows[row] or
+                    board[row][col] in cols[col] or
+                    board[row][col] in square[(row//3, col//3)]):
+                    return False
+                rows[row].add(board[row][col])
+                cols[col].add(board[row][col])
+                square[(row//3, col//3)].add(board[row][col])
+        return True
+
+sol = Solution()
+
+print(sol.isValidSudoku([
+    ["5","6",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+]))
