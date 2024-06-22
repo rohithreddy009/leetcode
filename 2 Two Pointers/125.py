ce0816ef1a13b5933a1d0185ba713d1a7c75d3bd
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-06-22 23:52:08 +0530

    125

---
+++ 2 Two Pointers/125.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new = ''
+        for a in s:
+            if a.isalpha() or a.isdigit():
+                new += a.lower()
+        return (new == new[::-1])
+
+a = Solution()
