450c5fd63d1c23d33c4467ec6eae4c82ff9cb5e4
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-07-06 20:03:12 +0530

    239

---
+++ 3 Sliding Window/239.py
@@ -0,0 +1,27 @@
+from typing import List
+import collections
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        l = r = 0
+        q = collections.deque()
+
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+        
+            if l > q[0]:
+                q.popleft()
+
+            if r + 1 >= k:
+                res.append(nums[q[0]])
+                l += 1
+            r += 1
+        return res
+        
+
+a = Solution()
+print(a.maxSlidingWindow([8,7,6,9],2))
