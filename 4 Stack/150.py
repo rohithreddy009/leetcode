1d8129e0e72724cba4f04170f73d13fa6c0602b5
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-07-08 22:13:05 +0530

    150

---
+++ 4 Stack/150.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
+
+
+a = Solution()
+print(a.evalRPN(["2","1","+","3","*"]))
