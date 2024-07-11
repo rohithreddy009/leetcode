37092510ed643896ab9a5f9959cafcff3abb5281
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-07-12 02:18:41 +0530

    150

--- 4 Stack/150.py
+++ 4 Stack/150.py
@@ -6,18 +6,26 @@ class Solution:
         for c in tokens:
             if c == "+":
                 stack.append(stack.pop() + stack.pop())
+
             elif c == "-":
-                a, b = stack.pop(), stack.pop()
+                a = stack.pop()
+                b = stack.pop()
                 stack.append(b - a)
+
             elif c == "*":
-                stack.append(stack.pop() * stack.pop())
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(a * b)
+
             elif c == "/":
-                a, b = stack.pop(), stack.pop()
-                stack.append(int(float(b) / a))
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(b/a))
+                
             else:
                 stack.append(int(c))
-        return stack[0]
 
+        return stack[0]
 
 a = Solution()
 print(a.evalRPN(["2","1","+","3","*"]))
