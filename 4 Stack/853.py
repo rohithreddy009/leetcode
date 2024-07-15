b0f55806e6e9be08cf35868e70d6800dae8f900e
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-07-15 19:28:18 +0530

    853

---
+++ 4 Stack/853.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+
+
+a = Solution()
+print(a.carFleet(100,[0,2,4],[4,2,1]))
