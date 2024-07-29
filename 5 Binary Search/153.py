d865a3b54864da2be3e72ed6a70e1ba2349f9155
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-07-29 17:26:58 +0530

    153

--- 5 Binary Search/153.py
+++ 5 Binary Search/153.py
@@ -2,22 +2,19 @@ from typing import List
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        start , end = 0, len(nums) - 1 
-        curr_min = float("inf")
-        
-        while start  <  end :
-            mid = start + (end - start ) // 2
-            curr_min = min(curr_min,nums[mid])
-            
-            # right has the min 
-            if nums[mid] > nums[end]:
-                start = mid + 1
-                
-            # left has the  min 
+        l, r = 0, len(nums) - 1
+        res = nums[l]
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+            mid = (l + r) // 2
+            res = min(res, nums[mid])
+            if nums[mid] >= nums[l]:
+                l = mid + 1
             else:
-                end = mid - 1 
-                
-        return min(curr_min,nums[start])
+                r = mid - 1
+        return res
 
 a = Solution()
 print(a.findMin([3,4,5,1,2]))
