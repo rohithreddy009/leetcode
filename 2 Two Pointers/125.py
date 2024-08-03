689994c229d8e0635a5c9cf92a1ebe4641f01882
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-08-03 23:54:34 +0530

    125

--- 2 Two Pointers/125.py
+++ 2 Two Pointers/125.py
@@ -1,9 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new = ''
-        for a in s:
-            if a.isalpha() or a.isdigit():
-                new += a.lower()
-        return (new == new[::-1])
+        def helper(x):
+            return ord('a') <= ord(x) <= ord('z') or ord('A') <= ord(x) <= ord('Z') or ord('0') <= ord(x) <= ord('9') 
+
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not helper(s[l]):
+                l += 1
+            while r > l and not helper(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l,r = l+1, r-1
+        return True
 
 a = Solution()
+print(a.isPalindrome("A man, a plan, a canal: Panama"))
