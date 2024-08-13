9ed08aeaea98fb6255836623d922bbc127862544
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-08-14 00:16:51 +0530

    206

--- 6 Linked List/206.py
+++ 6 Linked List/206.py
@@ -14,4 +14,25 @@ class Solution:
             current = next_node       # Move to the next node in the original list
         return prev  # prev will be the new head of the reversed list
 
+# Recursive
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next 
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+        
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return newHead
+
+        #recursive
+        
+
 
