b743f8b0a90576dc43539087f82934731a75e11c
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-10-07 22:18:36 +0530

    1046

--- 8 Heaps/1046.py
+++ 8 Heaps/1046.py
@@ -3,30 +3,35 @@ from typing import List
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        stones = [-s for s in stones]
-        heapq.heapify(stones)
-
-        while len(stones) > 1:
-            first = heapq.heappop(stones)
-            second = heapq.heappop(stones)
-            if second > first:
-                heapq.heappush(stones, first - second)
-
-        stones.append(0)
-        return abs(stones[0])
-
-# There's a private _heapify_max method.
-# https://github.com/python/cpython/blob/1170d5a292b46f754cd29c245a040f1602f70301/Lib/heapq.py#L198
-class Solution(object):
-    def lastStoneWeight(self, stones):
-        heapq._heapify_max(stones)
-        while len(stones) > 1:
-            max_stone = heapq._heappop_max(stones)
-            diff = max_stone - stones[0]
-            if diff:
-                heapq._heapreplace_max(stones, diff)
-            else:
-                heapq._heappop_max(stones)
+        def largest():
+            largest = stones.index(max(stones))
+            return stones.pop(largest)
         
-        stones.append(0)
-        return stones[0]
+        while len(stones) > 1:
+            x = largest()
+            y = largest()
+
+            if x != y:
+                stones.append(abs(x - y))
+            
+        return stones[0] if stones else 0
+
+
+a = Solution()
+print(a.lastStoneWeight([2,7,4,1,8,1]))
+
+# # There's a private _heapify_max method.
+# # https://github.com/python/cpython/blob/1170d5a292b46f754cd29c245a040f1602f70301/Lib/heapq.py#L198
+# class Solution(object):
+#     def lastStoneWeight(self, stones):
+#         heapq._heapify_max(stones)
+#         while len(stones) > 1:
+#             max_stone = heapq._heappop_max(stones)
+#             diff = max_stone - stones[0]
+#             if diff:
+#                 heapq._heapreplace_max(stones, diff)
+#             else:
+#                 heapq._heappop_max(stones)
+        
+#         stones.append(0)
+#         return stones[0]
