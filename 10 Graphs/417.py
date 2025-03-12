a41136f82c1bb2220b13a32c0efe1848a3110d30
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2025-03-12 23:57:48 +0530

    417

--- 10 Graphs/417.py
+++ 10 Graphs/417.py
@@ -33,6 +33,11 @@ class Solution:
         return res
 
 a = Solution()
-print(a.pacificAtlantic([[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]))
+# print(a.pacificAtlantic([[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]))
+print(a.pacificAtlantic([
+  [4,2,7,3,4],
+  [7,4,6,4,7],
+  [6,3,5,3,6]
+]))
 
         
