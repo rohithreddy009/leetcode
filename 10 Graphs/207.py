fe5aaa2053f5412b3b270193d57054e7afa64c63
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2025-03-20 16:08:25 +0530

    207

---
+++ 10 Graphs/207.py
@@ -0,0 +1,30 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqs = defaultdict(list)
+        seen = set()
+
+        for c, p in prerequisites:
+            prereqs[c].append(p)
+        
+        def dfs(course, seen):
+            if course in seen:
+                return True
+            seen.add(course)
+            for p in prereqs[course]:
+                if dfs(p, seen):
+                    return True
+            prereqs[course] = []
+            seen.remove(course)
+            return False
+
+        for course in range(numCourses):
+            if dfs(course, seen):
+                return False
+        return True
+
+a = Solution()     
+print(a.canFinish(4, [[0,2], [2,1], [0,1], [3,0]])) # True
+# print(a.canFinish(3, [[1,0],[2,1],[0,2],[2,0]])) # False
