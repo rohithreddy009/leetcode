bd65ba1162df17b52f6f706963ddf0dea9740c9b
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2025-05-19 17:20:23 +0530

    198

--- 12 DP/198.py
+++ 12 DP/198.py
@@ -4,6 +4,12 @@ from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
+        
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+
         memo = { 0: nums[0], 1: max(nums[0], nums[1])}
 
         def dfs(i):
