e6167e37f388105f58f282d937a1f29b6156f279
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2025-05-19 18:03:39 +0530

    213

---
+++ 12 DP/213.py
@@ -0,0 +1,58 @@
+from typing import List
+
+# top down
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        def hr2(nums):
+            n = len(nums)
+            
+            if n == 1:
+                return nums[0]
+            if n == 2:
+                return max(nums[0], nums[1])
+
+            memo = { 0: nums[0], 1: max(nums[0], nums[1])}
+
+            def dfs(i):
+                if i in memo:
+                    return memo[i]
+                else:
+                    memo[i] = max(nums[i] + dfs(i-2), dfs(i-1))
+                    return memo[i]
+            
+            return dfs(n-1)
+        
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        return max( hr2(nums[:-1]), hr2(nums[1:]))
+
+# bottom up
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        def hr2(nums):
+            n = len(nums)
+
+            if n == 1:
+                return nums[0]
+            if n == 2:
+                return max(nums[0], nums[1])
+
+            dp = [0] * n
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+
+            for i in range(2, n):
+                dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+            return dp[n-1]
+        
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        return max( hr2(nums[:-1]), hr2(nums[1:]))
+
+print(Solution().rob([2,3,2])) # 3
+print(Solution().rob([1,2,3,1])) # 4
+print(Solution().rob([1,2,3])) # 3  
