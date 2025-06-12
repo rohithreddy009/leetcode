6f35291c8d727c64f7cfc06f309b31625b4ce794
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2025-06-12 22:37:35 +0530

    322

--- 12 DP/1D/322.py
+++ 12 DP/1D/322.py
@@ -18,4 +18,20 @@ class Solution:
         
         return -1 if dfs(amount) >= float("inf") else dfs(amount)
 
-print(Solution().coinChange([1, 2, 5], 11))  # Output: 3
+class Solution2:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount+1] * (amount+1)
+        dp[0] =0
+
+        for i in range(1, amount +1):
+            for coin in coins:
+                if (i -coin) >= 0:
+
+                    dp[i] = min(dp[i], 1+ dp[i-coin])
+
+        if dp[amount] == amount + 1:
+            return -1
+        else:
+            return dp[amount]
+
+print(Solution2().coinChange([1, 2, 5], 6))  # Output: 3
