4498cedf5db0b6e2319e8e0a0177c70a5245c2f1
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2025-11-14 17:50:43 -0800

    236

---
+++ 7 Trees/236.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        def dfs(root, p, q):
+            if not root:
+                return None
+            if root == p or root == q:
+                return root
+            
+            l = dfs(root.left, p, q)
+            r = dfs(root.right, p, q)
+
+            if l and r:
+                return root
+            else:
+                return l or r
+        
+        return dfs(root, p, q)
